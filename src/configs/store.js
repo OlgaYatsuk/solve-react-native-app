f4bcc6d714cfd0dcd79b540c4134524da0e22cbf
olga <olgaa.yatsuk@gmail.com>
2019-10-25 11:55:35 +0300

    added logger;

--- src/configs/store.js
+++ src/configs/store.js
@@ -1,7 +1,8 @@
 // @flow
 
-import {createStore, combineReducers, applyMiddleware} from 'redux';
+import {createStore, combineReducers, compose, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
+import {createLogger} from 'redux-logger'
 import {cardDataReducer} from '../reducers/cardDataReducer';
 import {validationStatusReducer} from '../reducers/validationStatusReducer';
 
@@ -10,6 +11,13 @@ const rootReducer = combineReducers({
   validationStatusReducer,
 });
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const logger = createLogger({
+  collapsed: true,
+  diff: true,
+});
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 export default store;
